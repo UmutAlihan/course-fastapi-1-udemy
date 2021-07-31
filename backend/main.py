90b92c9383c27b5c3f5e1f99388dc8b7d04b9912
Alihan <averneus@gmail.com>
2021-07-31 14:44:48 +0000

    finished section 2 episode 17 & implement psgq ORM

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
-from db.base_class import Base
+from db.base import Base, Job, User
 
 
 def start_application():
-    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
     create_tables()
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
     return app
 
 def create_tables():
-     Base.metadata.create_all(bind=engine)
+    print("creating tables")
+    Base.metadata.create_all(bind=engine)
 
 app = start_application()
 
 @app.get("/")
 def hello_api():
     return {"detail": "Hello"}
+    
