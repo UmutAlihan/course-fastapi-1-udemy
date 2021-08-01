46c1cce679b5ef39b53605f719f4085fdc2335cd
Alihan <averneus@gmail.com>
2021-08-01 11:46:36 +0000

    section 2 ch 21 commit

---
+++ backend/core/hashing.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Hasher():
+    @staticmethod # you dont need to create an object if its staticmethod (no need for H = Hasher())
+    def verify_password(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
+    
+    @staticmethod
+    def get_password_hash(plain_password):
+        return pwd_context.hash(plain_password)
+
+
