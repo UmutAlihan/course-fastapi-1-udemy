2dd43d687548056bb021735a810b9affe98ffcd1
Alihan <averneus@gmail.com>
2021-08-01 15:09:53 +0000

    implement create-jobs endpoint

--- backend/core/config.py
+++ backend/core/config.py
@@ -13,7 +13,7 @@ class Settings:
     DATABASE_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "pass")
     DATABASE_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     DATABASE_PORT: str = os.getenv("POSTGRES_PORT", 5432)
-    DATABASE_DB: str = os.getenv("POSTGRES_DB")
+    DATABASE_DB: str = os.getenv("POSTGRES_DB", "db_course")
     DATABASE_URL: str = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_SERVER}:{DATABASE_PORT}"
 
 
