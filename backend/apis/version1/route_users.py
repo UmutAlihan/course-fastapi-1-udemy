cb0cb28779db50241d0d6b430d149a251a6441bf
Alihan <averneus@gmail.com>
2021-08-01 13:24:33 +0000

    bugfix on router refactor

--- backend/apis/version1/route_users.py
+++ backend/apis/version1/route_users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from schemas.users import UserCreate
+from schemas.users import UserCreate, ShowUser
 from db.session import get_db
 from db.repository.users import create_new_user
 
@@ -9,7 +9,7 @@ from db.repository.users import create_new_user
 router = APIRouter()
 
 
-@router.post("/")
+@router.post("/", response_model=ShowUser)
 def create_user(user: UserCreate, db:Session=Depends(get_db)):
     user = create_new_user(user, db)
     return user
--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -6,4 +6,13 @@ class UserCreate(BaseModel):
     username : str
     email : EmailStr
     password : str
+
+class ShowUser(BaseModel):
+    username : str
+    email : EmailStr
+    is_active : bool
+
+    class Config():
+        orm_mode = True
+        
     
