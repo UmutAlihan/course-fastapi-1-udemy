3a2ec564689114955926c774399245f5f30bb44d
Alihan <averneus@gmail.com>
2021-08-14 13:12:04 +0000

    fix test_users issue

--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -13,3 +13,17 @@ def test_create_job(client):
     response = client.post("/job/create-job",json.dumps(data))
     assert response.status_code == 200 or 307
 
+
+def test_retreive_job_by_id(client):
+    data = {
+        "title": "SDE 1 Yahoo",
+        "company": "testhoo",
+        "company_url": "https://www.asd.com",
+        "location": "USA,NY",
+        "description": "Testing",
+        "date_posted": "2022-08-01"
+    }
+
+    client.post("/job/create-job",json.dumps(data))
+    response = client.get("/job/")
+    
