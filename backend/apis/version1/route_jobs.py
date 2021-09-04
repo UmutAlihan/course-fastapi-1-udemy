dcbfb67462fad6933b236839378fa21a1af13530
Alihan <averneus@gmail.com>
2021-09-04 16:46:05 +0000

    authorization (permission) is implemented

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -3,19 +3,21 @@ from sqlalchemy.orm import Session
 
 from db.session import get_db
 from db.models.jobs import Job
+from db.models.users import User
 from schemas.jobs import JobCreate, ShowJob
 from db.repository.jobs import (create_new_job, 
                                 retreive_job, 
                                 list_jobs, 
                                 update_job_by_id, 
                                 delete_job_by_id)
+from apis.version1.route_login import get_current_user_from_token
 from typing import List
 
 router = APIRouter()
 
 @router.post("/create-job", response_model=ShowJob)
-def create_job(job : JobCreate, db : Session = Depends(get_db)):
-    owner_id = 1
+def create_job(job : JobCreate, db : Session = Depends(get_db), current_user: User=Depends(get_current_user_from_token)):
+    owner_id = current_user.id
     job = create_new_job(job=job, db=db, owner_id=owner_id)
     return job
 
--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -1,8 +1,9 @@
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from datetime import timedelta
+from jose import jwt, JWTError
 
 from db.session import get_db
 from db.repository.login import get_user
@@ -33,3 +34,23 @@ def login_for_access_token(form_data:OAuth2PasswordRequestForm = Depends(),
     access_token_expire = timedelta(minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub":user.email}, expires_delta=access_token_expire)
     return {"access_token":access_token, "token_type": "bearer"}
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+
+def get_current_user_from_token(token:str = Depends(oauth2_scheme), db:Session=Depends(get_db)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                            detail="Could not validate credentials")
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username:str = payload.get("sub")
+        print("email is", username)
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=username, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
+
