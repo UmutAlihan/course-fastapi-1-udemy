e3d82b20c44c2f8df8f0afff1e3f5403a40efc35
Alihan <averneus@gmail.com>
2021-09-11 13:43:03 +0000

    listview of jobs

--- backend/main.py
+++ backend/main.py
@@ -4,19 +4,29 @@ from db.session import engine
 from db.base import Base, Job, User
 from apis.base import api_router
 from webapps.base import api_router as webapp_router
+from fastapi.staticfiles import StaticFiles
+
+
+def create_tables():
+    print("creating tables")
+    Base.metadata.create_all(bind=engine)
+
 
 def include_router(app):
     app.include_router(api_router)
     app.include_router(webapp_router)
 
+
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 def start_application():
     create_tables()
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
     return app
 
-def create_tables():
-    print("creating tables")
-    Base.metadata.create_all(bind=engine)
 
 app = start_application()
--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -1,12 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from db.repository.jobs import list_jobs
+from sqlalchemy.orm import Session
+from db.session import get_db
+from fastapi import Depends
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(include_in_schema=False)
 
 
 @router.get("/")
-def homepage(request:Request):
-    return templates.TemplateResponse("jobs/homepage.html", {"request":request})
+def homepage(request:Request, db:Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return templates.TemplateResponse("jobs/homepage.html", {"request":request, "jobs": jobs})
