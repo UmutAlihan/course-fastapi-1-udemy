9ecb96e652065ea8565482f3317bdbbaa7957fc6
Alihan <averneus@gmail.com>
2021-10-10 15:16:58 +0000

    implement create-a-job-post endpoint, its form, template handling, backend logic

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -1,11 +1,18 @@
+from fastapi import Depends
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from db.repository.jobs import list_jobs
 from sqlalchemy.orm import Session
+from fastapi import responses, status
+from fastapi.security.utils import get_authorization_scheme_param
+
+from db.repository.jobs import list_jobs, create_new_job  
 from db.session import get_db
 from db.repository.jobs import retreive_job
-from fastapi import Depends
+from db.models.users import User
+from apis.version1.route_login import get_current_user_from_token
+from schemas.jobs import JobCreate
+from webapps.jobs.forms import JobCreateForm
 
 
 templates = Jinja2Templates(directory="templates")
@@ -22,3 +29,40 @@ def job_detail(id:int, request:Request, db:Session = Depends(get_db)):
     job = retreive_job(id=id, db=db)
     return templates.TemplateResponse("jobs/detail.html", 
                                      {"request":request, "job":job})
+
+
+@router.get("/post-a-job/")  
+def create_job(request: Request, db: Session = Depends(get_db)):
+    return templates.TemplateResponse("jobs/create_job.html", {"request": request})
+
+@router.post("/post-a-job/")
+async def create_job(request: Request, db: Session = Depends(get_db)):
+    # this is cleaner once you implement an UI 
+    #async def create_job(request: Request, db: Session = Depends(get_db),
+    #       current_user:User = Depends(get_current_user_from_token)):
+
+    # If you don't want to publish a job post instantly and review it before publishing,
+    # You can make use of the is_active field on the Job Model. When someone non-admin creates a job post it can be False by default. 
+    # And won't show up because in the list view we are querying from database
+    form = JobCreateForm(request)
+    await form.load_data()
+    if form.is_valid():
+        try:
+            token = request.cookies.get("access_token")
+            scheme, param = get_authorization_scheme_param(
+                token
+            )  # scheme will hold "Bearer" and param will hold actual token value
+            current_user: User = get_current_user_from_token(token=param, db=db)
+            job = JobCreate(**form.__dict__)
+            job = create_new_job(job=job, db=db, owner_id=current_user.id)
+            return responses.RedirectResponse(
+                f"/detail/{job.id}", status_code=status.HTTP_302_FOUND
+            )
+        except Exception as e:
+            print(e)
+            form.__dict__.get("errors").append(
+                "You might not be logged in, In case problem persists please contact us."
+            )
+            return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
+    return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
+
