8cbf6f3331ecc002bbcdf7aac0838e64ff74fd22
Alihan <averneus@gmail.com>
2021-10-10 14:41:02 +0000

    implement login logic with another endpoint as await func

--- backend/webapps/auth/route_login.py
+++ backend/webapps/auth/route_login.py
@@ -1,16 +1,37 @@
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
+from fastapi import Request, HTTPException
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
+from db.session import get_db
+from webapps.auth.forms import LoginForm
+from apis.version1.route_login import login_for_access_token
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(include_in_schema=False)
 
 
-@router.post("/login")
+@router.get("/login/")
 def login(request: Request):
     return templates.TemplateResponse("auth/login.html", {"request": request})
 
 
-
-
+@router.post("/login/")
+async def login(request: Request, db: Session = Depends(get_db)):
+    form = LoginForm(request)
+    await form.load_data()
+    if await form.is_valid():
+        try:
+            # since response already exists from login_for_access_token in /token endpoint, will be using that data
+            form.__dict__.update(msg="Login Successful :)")
+            response = templates.TemplateResponse("auth/login.html", form.__dict__)
+            login_for_access_token(response=response, form_data=form, db=db)
+            return response
+        except HTTPException:
+            # assumption in everything worked 'Login Successful' -> clearing it with empty string
+            form.__dict__.update(msg="")
+            # append error detail into 'error' list
+            form.__dict__.get("errors").append("Incorrect email and password")
+            return templates.TemplateResponse("auth/login.html", form.__dict__)
+    return templates.TemplateResponse("auth/login.html", form.__dict__)
